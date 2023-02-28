e8aab3f4ffd4f3fdf3d1e967463862c2b13359cf
Sivakumar S <sivakumarit42@gmail.com>
2023-02-28 16:37:10 +0530

    changes

--- vars.tf
+++ vars.tf
@@ -1,12 +1,48 @@
 variable "instances" {
-  default = {                                #map of map
-    catalogue = {                            #catalogue and user are key:name and type are value
+  default = {
+    frontend = {
+      name = "frontend"
+      type = "t3.micro"
+    }
+    mongodb = {
+      name = "mongodb"
+      type = "t3.micro"
+    }
+    catalogue = {
       name = "catalogue"
       type = "t3.micro"
-    },
+    }
+    redis = {
+      name = "redis"
+      type = "t3.micro"
+    }
     user = {
       name = "user"
-      type = "t2.small"
+      type = "t3.micro"
+    }
+    cart = {
+      name = "cart"
+      type = "t3.micro"
+    }
+    mysql = {
+      name     = "mysql"
+      type     = "t3.micro"
+      password = "RoboShop@1"
+    }
+    shipping = {
+      name     = "shipping"
+      type     = "t3.micro"
+      password = "RoboShop@1"
+    }
+    rabbitmq = {
+      name     = "rabbitmq"
+      type     = "t3.micro"
+      password = "roboshop123"
+    }
+    payment = {
+      name     = "payment"
+      type     = "t3.micro"
+      password = "roboshop123"
     }
   }
 }
