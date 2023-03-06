930d05345699079c6168ffff2d16c389b9970897
Sivakumar S <sivakumarit42@gmail.com>
2023-03-06 21:53:15 +0530

    changes

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "ec2" {
   ami                    = data.aws_ami.ami.image_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.sg.id]
+  iam_instance_profile   = "${var.env}-${var.component}-role"
   tags = {
     Name = var.component
   }
