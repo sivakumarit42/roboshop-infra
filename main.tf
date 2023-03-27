9489d6294c8d31e314d89ce945a7f5e8c868496a
Sivakumar S <sivakumarit42@gmail.com>
2023-03-27 10:29:35 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source              = "git::https://github.com/sivakumarit42/tf-module-vpc.git"
+  source              = "git::https://github.com/raghudevopsb71/tf-module-vpc.git"
   env                 = var.env
   tags                = var.tags
   default_route_table = var.default_route_table
@@ -11,68 +11,70 @@ module "vpc" {
   private_subnets = each.value["private_subnets"]
 }
 
-//module "docdb" {
-//  source = "git::https://github.com/sivakumarit42/tf-module-docdb.git"
-//  env    = var.env
-//  tags   = var.tags
-//
-//  subnet_ids = local.db_subnet_ids
-//
-//  for_each                = var.docdb
-//  engine                  = each.value["engine"]
-//  engine_version          = each.value["engine_version"]
-//  backup_retention_period = each.value["backup_retention_period"]
-//  preferred_backup_window = each.value["preferred_backup_window"]
-//  skip_final_snapshot     = each.value["skip_final_snapshot"]
-//  no_of_instances         = each.value["no_of_instances"]
-//  instance_class          = each.value["instance_class"]
-//}
-//
-//module "rds" {
-//  source = "git::https://github.com/sivakumarit42/tf-module-rds.git"
-//  env    = var.env
-//  tags   = var.tags
-//
-//  subnet_ids = local.db_subnet_ids
-//
-//  for_each                = var.rds
-//  engine                  = each.value["engine"]
-//  backup_retention_period = each.value["backup_retention_period"]
-//  preferred_backup_window = each.value["preferred_backup_window"]
-//  engine_version          = each.value["engine_version"]
-//  instance_class          = each.value["instance_class"]
-//  no_of_instances         = each.value["no_of_instances"]
-//}
-//
-//module "elasticache" {
-//  source = "git::https://github.com/sivakumarit42/tf-modue-elasticache.git"
-//  env    = var.env
-//  tags   = var.tags
-//
-//  subnet_ids = local.db_subnet_ids
-//
-//  for_each        = var.elasticache
-//  engine          = each.value["engine"]
-//  engine_version  = each.value["engine_version"]
-//  num_cache_nodes = each.value["num_cache_nodes"]
-//  node_type       = each.value["node_type"]
-//
-//}
-//
-//module "rabbitmq" {
-//  source = "git::https://github.com/sivakumarit42/tf-module-rabbitmq.git"
-//  env    = var.env
-//  tags   = var.tags
-//
-//  subnet_ids = local.db_subnet_ids
-//
-//  for_each      = var.rabbitmq
-//  instance_type = each.value["instance_type"]
-//
-//}
+module "docdb" {
+  source = "git::https://github.com/raghudevopsb71/tf-module-docdb.git"
+  env    = var.env
+  tags   = var.tags
+
+  subnet_ids = local.db_subnet_ids
+  vpc_id     = module.vpc["main"].vpc_id
+
+  for_each                = var.docdb
+  engine                  = each.value["engine"]
+  engine_version          = each.value["engine_version"]
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  skip_final_snapshot     = each.value["skip_final_snapshot"]
+  no_of_instances         = each.value["no_of_instances"]
+  instance_class          = each.value["instance_class"]
+  allow_subnets           = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
+}
+
+module "rds" {
+  source = "git::https://github.com/raghudevopsb71/tf-module-rds.git"
+  env    = var.env
+  tags   = var.tags
+
+  subnet_ids = local.db_subnet_ids
+
+  for_each                = var.rds
+  engine                  = each.value["engine"]
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  engine_version          = each.value["engine_version"]
+  instance_class          = each.value["instance_class"]
+  no_of_instances         = each.value["no_of_instances"]
+}
+
+module "elasticache" {
+  source = "git::https://github.com/raghudevopsb71/tf-module-elasticache.git"
+  env    = var.env
+  tags   = var.tags
+
+  subnet_ids = local.db_subnet_ids
+
+  for_each        = var.elasticache
+  engine          = each.value["engine"]
+  engine_version  = each.value["engine_version"]
+  num_cache_nodes = each.value["num_cache_nodes"]
+  node_type       = each.value["node_type"]
+
+}
+
+module "rabbitmq" {
+  source = "git::https://github.com/raghudevopsb71/tf-module-rabbitmq.git"
+  env    = var.env
+  tags   = var.tags
+
+  subnet_ids = local.db_subnet_ids
+
+  for_each      = var.rabbitmq
+  instance_type = each.value["instance_type"]
+
+}
 
 module "alb" {
-  source = "git::https://github.com/sivakumarit42/tf-module-alb.git"
+  source = "git::https://github.com/raghudevopsb71/tf-module-alb.git"
   env    = var.env
   tags   = var.tags
 
@@ -87,7 +89,7 @@ module "alb" {
 }
 
 module "app" {
-  source       = "git::https://github.com/sivakumarit42/tf-module-app.git"
+  source       = "git::https://github.com/raghudevopsb71/tf-module-app.git"
   env          = var.env
   tags         = var.tags
   bastion_cidr = var.bastion_cidr
@@ -110,5 +112,5 @@ module "app" {
 }
 
 output "alb" {
-  value = module.alb
+  value = module.elasticache
 }
